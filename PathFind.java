43e759a10469d4a96a174b2c74bb9d824f807e37
Mikolaj Figurski <meek.f80@gmail.com>
2018-12-05 19:55:23 -0500

    Added basic Node private Class

--- PathFind.java
+++ PathFind.java
@@ -1,5 +1,34 @@
 public class PathFind {
+
+  /**
+   * Class to hold individual point (city) that can be traveled through
+   * Constructor needs:
+   * @param x-position
+   * @param y-position
+   */
+  private static class Node {
+    public boolean isOpen = true;
+    public int x,y;
+    public int cost;
+    public Node parent;
+
+    public Node (int _x, int _y) {
+      x = _x;
+      y = _y;
+    }
+
+    public static double getDistanceFromPoint(int targetX, int targetY) {
+      return(4.3); // TODO: write getDistanceFromPoint method
+    }
+  }
+
+
+  /**
+   * MAIN CLASS BEGINS HERE
+   * @param args
+   */
   public static void main(String[] args) {
     System.out.println("Edited from IntelliJ");
+    Node n = new Node(1,1);
   }
 }
